183fc5c6ad52a59ae31863c6c83291c8886c5398
Vivekkr83 <vivekkr8316@gmail.com>
2023-11-18 19:42:37 +0530

    Update ComicComponent.js

--- dash-app/src/ComicComponent.js
+++ dash-app/src/ComicComponent.js
@@ -1,91 +1,29 @@
-import React from "react";
-import "./styles2.css";
-
-const ComicComponent = (props) => {
-  return (
-    <div className="comic">
-      <div className="panel">
-        <p className="text top-left">Begin...</p>
-        <p className="text bottom-right">...next photo</p>
-      </div>
-      <div className="panel"></div>
-      <div className="panel"></div>
-      <div className="panel">
-        <p className="text bottom-right">...</p>
-        <p className="speech">A speech bubble</p>
-      </div>
-      <div className="panel"></div>
-      <div className="panel">
-        <p className="text top-left">...</p>
-      </div>
-      <div className="panel">
-        <p className="text bottom-right">IRON MAN</p>
-      </div>
-      <div className="panel"></div>
-      <div className="panel"></div>
-      <div className="panel">
-        <p className="text bottom-right">THE END</p>
-      </div>
-    </div>
-  );
-};
-
-export default ComicComponent;
-
 // import React from "react";
 // import "./styles2.css";
 
 // const ComicComponent = (props) => {
 //   return (
 //     <div className="comic">
-//       <div
-//         className="panel"
-//         style={{ backgroundImage: `url(${props.imageUrls[1]})` }}
-//       >
+//       <div className="panel">
 //         <p className="text top-left">Begin...</p>
 //         <p className="text bottom-right">...next photo</p>
 //       </div>
-//       <div
-//         className="panel"
-//         style={{ backgroundImage: `url(${props.imageUrls[2]})` }}
-//       ></div>
-//       <div
-//         className="panel"
-//         style={{ backgroundImage: `url(${props.imageUrls[3]})` }}
-//       ></div>
-//       <div
-//         className="panel"
-//         style={{ backgroundImage: `url(${props.imageUrls[4]})` }}
-//       >
+//       <div className="panel"></div>
+//       <div className="panel"></div>
+//       <div className="panel">
 //         <p className="text bottom-right">...</p>
 //         <p className="speech">A speech bubble</p>
 //       </div>
-//       <div
-//         className="panel"
-//         style={{ backgroundImage: `url(${props.imageUrls[5]})` }}
-//       ></div>
-//       <div
-//         className="panel"
-//         style={{ backgroundImage: `url(${props.imageUrls[6]})` }}
-//       >
+//       <div className="panel"></div>
+//       <div className="panel">
 //         <p className="text top-left">...</p>
 //       </div>
-//       <div
-//         className="panel"
-//         style={{ backgroundImage: `url(${props.imageUrls[7]})` }}
-//       ></div>
-//       <div
-//         className="panel"
-//         style={{ backgroundImage: `url(${props.imageUrls[8]})` }}
-//       ></div>
-//       <div
-//         className="panel"
-//         style={{ backgroundImage: `url(${props.imageUrls[9]})` }}
-//       ></div>
-//       <div
-//         className="panel"
-//         style={{ backgroundImage: `url(${props.imageUrls[10]})` }}
-//       >
+//       <div className="panel">
+//         <p className="text bottom-right">IRON MAN</p>
+//       </div>
+//       <div className="panel"></div>
+//       <div className="panel"></div>
+//       <div className="panel">
 //         <p className="text bottom-right">THE END</p>
 //       </div>
 //     </div>
@@ -93,3 +31,65 @@ export default ComicComponent;
 // };
 
 // export default ComicComponent;
+
+import React from "react";
+import "./styles2.css";
+
+const ComicComponent = (props) => {
+  return (
+    <div className="comic">
+      <div
+        className="panel"
+        style={{ backgroundImage: `url(${props.imageUrls[1]})` }}
+      >
+        <p className="text top-left">Begin...</p>
+        <p className="text bottom-right">...next photo</p>
+      </div>
+      <div
+        className="panel"
+        style={{ backgroundImage: `url(${props.imageUrls[2]})` }}
+      ></div>
+      <div
+        className="panel"
+        style={{ backgroundImage: `url(${props.imageUrls[3]})` }}
+      ></div>
+      <div
+        className="panel"
+        style={{ backgroundImage: `url(${props.imageUrls[4]})` }}
+      >
+        <p className="text bottom-right">...</p>
+        <p className="speech">A speech bubble</p>
+      </div>
+      <div
+        className="panel"
+        style={{ backgroundImage: `url(${props.imageUrls[5]})` }}
+      ></div>
+      <div
+        className="panel"
+        style={{ backgroundImage: `url(${props.imageUrls[6]})` }}
+      >
+        <p className="text top-left">...</p>
+      </div>
+      <div
+        className="panel"
+        style={{ backgroundImage: `url(${props.imageUrls[7]})` }}
+      ></div>
+      <div
+        className="panel"
+        style={{ backgroundImage: `url(${props.imageUrls[8]})` }}
+      ></div>
+      <div
+        className="panel"
+        style={{ backgroundImage: `url(${props.imageUrls[9]})` }}
+      ></div>
+      <div
+        className="panel"
+        style={{ backgroundImage: `url(${props.imageUrls[10]})` }}
+      >
+        <p className="text bottom-right">THE END</p>
+      </div>
+    </div>
+  );
+};
+
+export default ComicComponent;
